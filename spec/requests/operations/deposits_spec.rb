ab6c8db4017801952e3994806aabc88db634ef92
Rui Andrada <shingonoide@gmail.com>
2022-07-20 20:05:08 -0300

    Prepare initial specs

---
+++ spec/requests/operations/deposits_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe "Operations::Deposits", type: :request do
+  describe "GET /operations/deposits/new" do
+    it "Just works!" do
+      get new_operations_deposit_path
+      expect(response).to have_http_status(200)
+    end
+  end
+end
--- spec/requests/operations_spec.rb
+++ spec/requests/operations_spec.rb
@@ -1,23 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe "Operations", type: :request do
-  describe "GET /deposit" do
+  describe "GET /" do
     it "returns http success" do
-      get "/operations/deposit"
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET /withdraw" do
-    it "returns http success" do
-      get "/operations/withdraw"
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET /transfer" do
-    it "returns http success" do
-      get "/operations/transfer"
+      get operations_path
       expect(response).to have_http_status(:success)
     end
   end
